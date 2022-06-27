32c58cadcf979dda1a2d4bf39696129184c94257
safym <osafym@gmail.com>
2022-06-27 03:00:55 +0300

    гет запрос к уровню вопросов и доступность инпутов в зависимости от других

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,9 +12,9 @@ function App() {
         <p className='logo'>IQG ?</p>
       </header>
 
-      <div className='mainContainer'>
-        <InputsForm />
+      <div className="mainContainer">
 
+        <InputsForm />
 
         <div className='questions'>
           <QuestionItem />
@@ -31,46 +31,66 @@ function App() {
 const InputsForm = () => {
   const [formData, setFormData] = useState({
     job: {
-      id: null,
+      id: "default",
       name: ""
     },
-    level: "",
+    level: {
+      id: "default",
+      name: ""
+    },
     count: null
   })
 
   const handleSubmit = (e) => {
     e.preventDefault()
     console.log(formData)
-}
+  }
 
+  const handleChangeJob = (e) => {
+    setFormData({ ...formData, job: { id: e.target.selectedOptions[0].id, name: e.target.value } });
+  }
 
-  return(
+  const handleChangeLevel = (e) => {
+    setFormData({ ...formData, level: { id: e.target.selectedOptions[0].id, name: e.target.value } });
+  }
+
+  const handleChangeCount= (e) => {
+    setFormData({ ...formData, count: e.target.value });
+  }
+
+  useEffect(() => {
+    var level = document.getElementById("level");
+    var count = document.getElementById("count");
+
+    console.log(formData.level.name)
+    if (formData.level.name == "Сложность" || formData.level.name == "") {
+      count.disabled = true;
+    }
+    else {
+      count.disabled = false;
+    }
+
+    if (formData.job.id == null || formData.job.id == "default") {
+      level.disabled = true;
+      count.disabled = true; 
+    }
+    else {
+      level.disabled = false;
+    }
+
+  })
+
+  return (
     <form className="inputs" onSubmit={handleSubmit}>
 
-          <SelectJob callback={(e) => setFormData({...formData, job: {id: e.target.selectedOptions[0].id, name: e.target.value}})}/>
+      <SelectJob callback={handleChangeJob} />
+      <SelectLevel job={formData.job.id} callback={handleChangeLevel} />
+      <SelectCount callback={handleChangeCount} />
 
-          <div className="selectLevel">
-            <select className="test-input" onChange={(e) => setFormData({...formData, level: e.target.value})}>
-              <option value="default">Сложность</option>
-              <option value="junior">Junior</option>
-              <option value="middle">Middle</option>
-              <option value="senior">Senior</option>
-            </select>
-          </div>
-
-          <div className="selectCount">
-            <select className='test-input' onChange={(e) => setFormData({...formData, count: e.target.value})} disabled>
-              <option value="default">Количество вопросов</option>
-              <option value="5">5</option>
-              <option value="10">10</option>
-              <option value="15">15</option>
-            </select>
-          </div>
-
-          <div className="generate">
-            <input className="btn" value="Сгенерировать" type="submit" />
-          </div>
-        </form>
+      <div className="generate">
+        <input className="btn" value="Сгенерировать" type="submit" />
+      </div>
+    </form>
 
   )
 }
@@ -97,7 +117,6 @@ const SelectJob = (props) => {
   }, [])
 
   const handleChange = (event) => {
-    console.log(event)
     props.callback(event)
   }
 
@@ -121,6 +140,76 @@ const SelectJob = (props) => {
       </div>
     );
   }
+}
+
+const SelectLevel = (props) => {
+
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    if (props.job != "default") {
+
+      fetch("http://localhost:5000/iqg/api/jobs/" + String(props.job) + "/levels")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            setItems(result.data);
+          },
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        )
+    }
+  }, [props.job])
+
+  const handleChange = (event) => {
+    props.callback(event)
+  }
+
+  if (error) {
+    return <div>Ошибка: {error.message}</div>;
+  }
+  // else if (!isLoaded) {
+  //   return <div>Загрузка...</div>;
+  // } 
+  else {
+    return (
+      <div className="selectLevel">
+        <select className="test-input" id="level" onChange={handleChange}>
+          <option key={"default"} id="default">
+            Сложность
+          </option>
+          {items.map(data => (
+            <option value={data.name}>
+              {data.name.charAt(0).toUpperCase() + data.name.slice(1)}
+            </option>
+          ))}
+        </select>
+      </div>
+    );
+  }
+}
+
+const SelectCount = (props) => {
+
+  const handleChange = (event) => {
+    props.callback(event)
+  }
+
+  return (
+    <div className="selectCount">
+      <select className='test-input' id="count" onChange={handleChange} disabled>
+        <option value="default">Количество вопросов</option>
+        <option value="5">5</option>
+        <option value="10">10</option>
+        <option value="15">15</option>
+      </select>
+    </div>
+  );
 }
 
 const QuestionItem = () => {
